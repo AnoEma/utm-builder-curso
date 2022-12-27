5b393301f1d56f6cb2bd951d3a97f88650fc4126
AnoEma <anoemangi@gmail.com>
2022-12-27 19:35:56 -0300

    Add ListExtension

---
+++ UtmBuilder.core/Extensions/ListExtensions.cs
@@ -0,0 +1,13 @@
+namespace UtmBuilder.core.Extensions;
+
+public static class ListExtension
+{
+    public static void AddIfNotNull(
+        this List<string> list,
+        string key,
+        string? value)
+    {
+        if(!string.IsNullOrEmpty(value))
+            list.Add($"{key}={value}");
+    }
+}
